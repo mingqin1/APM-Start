bbc200aad7e406ee5f9a20d1c6eaec102bcbf9a8
Ming Qin <mingqin@live.com>
2018-04-06 08:37:17 -0700

    Revert "ming's change"
    This reverts commit fea26cfe21b76764b6610d35727481e4e2afbc80.

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -192,7 +192,7 @@ export class ProductService {
         return this.http.get<IProduct[]>(this.productsUrl)
             .pipe(
 
-                ming change  tap(data => console.log(JSON.stringify(data))),
+                I am change tap(data => console.log(JSON.stringify(data))),
 
               
                 catchError(this.handleError)
