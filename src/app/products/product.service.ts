2f016061eef8054eee8309362e5294ba99328bdd
Ming Qin <mingqin@live.com>
2018-04-06 19:46:29 -0700

    ming's changes

--- src/app/products/product.service.ts
+++ src/app/products/product.service.ts
@@ -191,7 +191,7 @@ export class ProductService {
     getProducts(): Observable<IProduct[]> {
         return this.http.get<IProduct[]>(this.productsUrl)
             .pipe(
-             simi valley's changes2 tap(data => console.log(JSON.stringify(data))),
+             ming's change tap(data => console.log(JSON.stringify(data))),
                 catchError(this.handleError)
             );
     }
